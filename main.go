c5a2352fd1498ada71ce4da3337519e358fa4fba
dzdn504 <enudoko24@gmail.com>
2018-12-04 15:20:41 -0700

    define data model

--- main.go
+++ main.go
@@ -17,6 +17,20 @@ PUT		/contacts/{id}	- update existing contact
 DELETE	/contacts/{id}	- delete contact
 */
 
+type Contact struct {
+	ID          string   `json:"id,omitempty`
+	FirstName   string   `json:"first_name,omitempty`
+	LastName    string   `json:"last_name,omitempty`
+	PhoneNumber string   `json:"phone_number,omitempty`
+	HomeAdress  *Address `json:"address,omitempty`
+}
+
+type Address struct {
+	City    string `json:"city,omitempty`
+	State   string `json:"state,omitempty`
+	ZipCode string `json:"zip_code,omitempty`
+}
+
 // GET all contacts
 func GetAllContactsEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(responseWriter, "not implemented yet !")
