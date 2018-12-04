b9ab185ce71bf3892e291c6ca7fd3301d577396d
dzdn504 <enudoko24@gmail.com>
2018-12-04 14:59:49 -0700

    define endpoints

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -15,6 +16,31 @@ POST 	/contacts/{id}	- create a new contact
 PUT		/contacts/{id}	- update existing contact
 DELETE	/contacts/{id}	- delete contact
 */
+
+// GET all contacts
+func GetAllContactsEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// GET a single contact with id
+func GetContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// POST a contact
+func CreateContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// PUT - update existing contact
+func UpdateContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
+// DELETE contact
+func DeleteContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
 
 func main() {
 	router := mux.NewRouter()
