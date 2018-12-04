cd63bbe68eac0e80d46b4d678fbe287fe5543f40
dzdn504 <enudoko24@gmail.com>
2018-12-04 15:13:21 -0700

    define routes, expose server port

--- main.go
+++ main.go
@@ -19,31 +19,36 @@ DELETE	/contacts/{id}	- delete contact
 
 // GET all contacts
 func GetAllContactsEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	fmt.Fprintln(responseWriter, "not implemented yet !")
 }
 
 // GET a single contact with id
 func GetContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	fmt.Fprintln(responseWriter, "not implemented yet !")
 }
 
 // POST a contact
 func CreateContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	fmt.Fprintln(responseWriter, "not implemented yet !")
 }
 
 // PUT - update existing contact
 func UpdateContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	fmt.Fprintln(responseWriter, "not implemented yet !")
 }
 
 // DELETE contact
 func DeleteContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	fmt.Fprintln(responseWriter, "not implemented yet !")
 }
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/contacts", GetAllContactsEndPoint).Methods("GET")
+	router.HandleFunc("/contacts/{id}", GetContactEndPoint).Methods("GET")
+	router.HandleFunc("/contacts/{id}", CreateContactEndPoint).Methods("POST")
+	router.HandleFunc("/contacts/{id}", UpdateContactEndPoint).Methods("PUT")
+	router.HandleFunc("/contacts/{id}", DeleteContactEndPoint).Methods("DELETE")
 	if err := http.ListenAndServe(":3000", router); err != nil {
 		log.Fatal(err)
 	}
