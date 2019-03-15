22b1442fdbf1060653428e65e05d4047f7e50a3b
dzdn504 <enudoko24@gmail.com>
2019-03-15 14:26:29 -0700

    implement GetContact endpoint

--- app.go
+++ app.go
@@ -9,12 +9,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// App ...
 type App struct {
 	Router *mux.Router
 }
 
 var contacts []Contact
 
+// InitializeContacts ...
 func InitializeContacts() {
 	contacts = append(contacts, Contact{
 		ID: "1", Name: "John Smith",
@@ -26,31 +28,45 @@ func InitializeContacts() {
 		ID: "3", Name: "Joe Biden"})
 }
 
+// GetAllContactsEndPoint ...
 // GET all contacts
-func GetAllContactsEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(responseWriter).Encode(contacts)
+func GetAllContactsEndPoint(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(contacts)
 }
 
+// GetContactEndPoint ...
 // GET a single contact with id
-func GetContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(responseWriter, "not implemented yet !")
+func GetContactEndPoint(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintln(w, "not implemented yet !")
+	params := mux.Vars(r)
+	for _, contact := range contacts {
+		if contact.ID == params["id"] {
+			json.NewEncoder(w).Encode(contact)
+			return
+		}
+	}
+	http.Error(w, fmt.Sprintf("Invalid contact id: %s", params["id"]), http.StatusBadRequest)
 }
 
+// CreateContactEndPoint ...
 // POST a contact
-func CreateContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(responseWriter, "not implemented yet !")
+func CreateContactEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
 }
 
+// UpdateContactEndPoint ...
 // PUT - update existing contact
-func UpdateContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(responseWriter, "not implemented yet !")
+func UpdateContactEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
 }
 
+// DeleteContactEndPoint ...
 // DELETE contact
-func DeleteContactEndPoint(responseWriter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(responseWriter, "not implemented yet !")
+func DeleteContactEndPoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
 }
 
+// Initialize ...
 func (a *App) Initialize() {
 	a.Router = mux.NewRouter()
 	a.Router.HandleFunc("/contacts", GetAllContactsEndPoint).Methods("GET")
@@ -63,6 +79,7 @@ func (a *App) Initialize() {
 
 }
 
+// Run ...
 func (a *App) Run(addr string) {
 
 	if err := http.ListenAndServe(addr, a.Router); err != nil {
